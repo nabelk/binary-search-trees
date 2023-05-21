bc415f2da5a79d1e9a8130d35b99825b5d9f0a3e
Nabil Khalid <holanabilkhalid@gmail.com>
2023-05-22 00:01:45 +0800

    Create node class

---
+++ nodeBST.js
@@ -0,0 +1,11 @@
+class Node {
+    constructor(data = null, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+module.exports = {
+    Node,
+};
